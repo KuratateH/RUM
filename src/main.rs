671e40e3069c8ad3ed42aa2fb7bbf8f536be9d60
KuratateH <166468864+KuratateH@users.noreply.github.com>
2024-05-14 11:49:18 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use archiver::{archiver_info, ArchiverOpts};
+use clap::Parser;
+use cli::*;
+use cli::{RunMode, ToteError};
+use extractor::{extractor_info, ExtractorOpts};
+
+mod archiver;
+mod cli;
+mod extractor;
+mod format;
+mod verboser;
+
 fn main() {
-    println!("Hello, world!");
+    let _opts = Cli0pts::parse();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cli::RunMode;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_run() {
+        let opts = CliOpts::parse_from(&[
+            "totebag_test",
+            "-o",
+            "test.zip",
+            "src",
+            "LICENSE",
+            "README.md",
+            "Cargo.toml",
+        ]);
+        assert_eq!(opts.mode, RunMode::Auto);
+        assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
+        assert_eq!(opts.args.len(), 4);
+        assert_eq!(
+            opts.args,
+            vec![
+                PathBuf::from("src"),
+                PathBuf::from("LICENSE"),
+                PathBuf::from("README.md"),
+                PathBuf::from("Cargo.toml")
+            ]
+        );
+    }
 }
